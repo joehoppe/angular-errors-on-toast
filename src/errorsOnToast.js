135384d2acce6c7b01f48e6d6fd0e25fab51df87
Joe Hoppe <jhoppe@gmail.com>
2015-01-25 20:53:13 -0500

    Initial commit
    Adding files

---
+++ src/errorsOnToast.js
@@ -0,0 +1,40 @@
+﻿//version 0.1
+(function () {
+    "use strict";
+    angular.module("errors-on-toast", [
+        // Angular modules         
+        // Custom modules 
+        // 3rd Party Modules
+        "toastr"
+    ]);
+})();
+(function () {
+    "use strict";
+    angular
+        .module("errors-on-toast")
+        .factory("runtimeErrorFactory", runtimeErrorFactory);
+
+    runtimeErrorFactory.$inject = ["$location", "toastr"];
+
+    function runtimeErrorFactory($location, toastr) {
+        var service = {
+            error: error
+        };
+
+        var host = $location.host();
+        var allowedUrls = ["localhost"];
+
+        function isUrlAllowed() {
+            return allowedUrls.length > 0
+                && allowedUrls.indexOf(host) !== -1;
+        }
+
+        function error(message) {
+            if (isUrlAllowed(host) === true) {
+                toastr.error(message, "Error");
+            }
+        }
+
+        return service;
+    }
+})();
---
+++ src/exceptionHandler.js
@@ -0,0 +1,12 @@
+﻿//version 0.1
+"use strict";
+//override the angular exception handler service. pop a toast to display error message
+angular.module("app").factory("$exceptionHandler", function ($injector) {
+    return function (exception) { //(exception, cause) {
+        var $log = $injector.get("$log");
+        $log.error.apply($log, arguments);
+
+        var runtimeErrorFactory = $injector.get("runtimeErrorFactory");
+        runtimeErrorFactory.error(exception.message);
+    };
+});
